5db1fa374864febafc0a05ef5894cba6d142f02e
LPAnhKhôi <lekhoi1492@gmail.com>
2024-10-05 15:55:06 +0700

    1

--- Buoi_12/Phan_01_.py
+++ Buoi_12/Phan_01_.py
@@ -25,35 +25,3 @@ cay = None
 for giaTri in debai:
     cay = chenNodetoCay(cay, giaTri)
 inCay(cay)
-
-#? Phần 02
-lstPhone =[
-	{
-		"name": "phone A",
-		"price": 3
-	},
-	{
-		"name": "phone B",
-		"price": 2
-	},
-	{
-		"name": "phone C",
-		"price": 1
-	},
-	{
-		"name": "phone D",
-		"price": 1
-	},
-	{
-		"name": "phone E",
-		"price": 4
-	},
-	{
-		"name": "phone F",
-		"price": 10
-	},
-	{
-		"name": "phone g",
-		"price": 22
-	}
-]
---
+++ Buoi_12/Phan_02_.py
@@ -0,0 +1,30 @@
+lstPhone =[
+	{
+		"name": "phone A",
+		"price": 3
+	},
+	{
+		"name": "phone B",
+		"price": 2
+	},
+	{
+		"name": "phone C",
+		"price": 1
+	},
+	{
+		"name": "phone D",
+		"price": 1
+	},
+	{
+		"name": "phone E",
+		"price": 4
+	},
+	{
+		"name": "phone F",
+		"price": 10
+	},
+	{
+		"name": "phone g",
+		"price": 22
+	}
+]
