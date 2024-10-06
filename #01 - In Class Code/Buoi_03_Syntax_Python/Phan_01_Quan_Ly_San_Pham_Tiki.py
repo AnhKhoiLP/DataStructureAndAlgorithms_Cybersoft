9b4d57ec7445d950e5867ae687641ff165cdabcc
LPAnhKhôi <lekhoi1492@gmail.com>
2024-10-06 17:06:27 +0700

    7

---
+++ #01 - In Class Code/Buoi_03_Syntax_Python/Phan_01_Quan_Ly_San_Pham_Tiki.py
@@ -0,0 +1,13 @@
+
+#> CÁCH 01: 
+
+
+ma_SP_01 	= input("Nhập Mã Sản Phẩm 01: ")
+ten_SP_01 	= input("Nhập Tên Sản Phẩm 01: ")
+ma_SP_02 	= input("Nhập Mã Sản Phẩm 02: ")
+ten_SP_02 	= input("Nhập Tên Sản Phẩm 02: ")
+ma_SP_03 	= input("Nhập Mã Sản Phẩm 03: ")
+ten_SP_03 	= input("Nhập Tên Sản Phẩm 03: ")
+print(ma_SP_01, " | ", ten_SP_01)
+print(ma_SP_02, " | ", ten_SP_02)
+print(ma_SP_03, " | ", ten_SP_03)
