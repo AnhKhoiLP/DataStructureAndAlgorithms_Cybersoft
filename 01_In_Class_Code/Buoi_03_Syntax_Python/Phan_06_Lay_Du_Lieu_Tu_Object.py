23dafc4ac44561d32a8f5edd65c0561ba8b4d421
LPAnhKhôi <lekhoi1492@gmail.com>
2024-10-12 23:30:26 +0700

    41

--- 01_In_Class_Code/Buoi_03_Syntax_Python/Phan_06_Lay_Du_Lieu_Tu_Object.py
+++ 01_In_Class_Code/Buoi_03_Syntax_Python/Phan_06_Lay_Du_Lieu_Tu_Object.py
@@ -4,8 +4,8 @@ san_pham_01 = {
 	"ma_SP"			: 1,
 	"ten_san_pham"	: "Sản Phẩm 01"
 }
+
+
 print(san_pham_01)
-
-
 print(san_pham_01["ma_SP"])
 print(san_pham_01["ten_san_pham"])
---
+++ 02_Homework/Buoi_02_Bai_02_.py
@@ -0,0 +1,31 @@
+import customtkinter as CTk
+
+#* Khởi Tạo Cửa Sổ Chính
+window = CTk.CTk()
+window.title("Lê Phước Anh Khôi")
+window.geometry("800x600")
+
+#* Người Dùng Không Được Phép Thay Đổi Kích Thước Cửa Sổ
+window.resizable(False, False)
+
+#* Tạo 2 Frame Cha Chứa Các Frame Con
+top_frame = CTk.CTkFrame(window)
+bottom_frame = CTk.CTkFrame(window)
+
+#* Custom Top + Bot Frame
+top_frame.pack(side = "top", fill = "both", expand = True)
+bottom_frame.pack(side = "bottom", fill = "both", expand = True)
+
+#* Tạo 4 Frame
+frame01 = CTk.CTkFrame(top_frame	, fg_color = "yellow")
+frame02 = CTk.CTkFrame(top_frame	, fg_color = "red")
+frame03 = CTk.CTkFrame(bottom_frame	, fg_color = "gray")
+frame04 = CTk.CTkFrame(bottom_frame	, fg_color = "blue")
+
+frame01.pack(side = "left", fill = "both", expand = True)		#? Frame 01 - Top.Left 		- Yellow
+frame02.pack(side = "left", fill = "both", expand = True)		#? Frame 02 - Top.Right 	- Red
+frame03.pack(side = "left", fill = "both", expand = True)		#? Frame 03 - Bottom.Left 	- Gray
+frame04.pack(side = "left", fill = "both", expand = True)		#? Frame 04 - Bottom.Right 	- Blue
+
+#* Chạy giao diện
+window.mainloop()
